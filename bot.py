c9162de1569c46f78f047c51739d9d56ab6034c8
tejpat67 <90096732+tejpat67@users.noreply.github.com>
2021-09-07 10:41:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,7 +31,7 @@ class Bot(Client):
             except:
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning("Please Double check the FORCE_SUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission")
-                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/CodeXBotzSupport for support")
+                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/tp_botz for support")
                 sys.exit()
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
@@ -45,7 +45,7 @@ class Bot(Client):
             sys.exit()
 
         self.set_parse_mode("html")
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by 𝘾𝙤𝙙𝙚 𝕏 𝘽𝙤𝙩𝙯\nhttps://t.me/CodeXBotz")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by Tp botz\nhttps://t.me/tp_botz")
         self.username = usr_bot_me.username
 
     async def stop(self, *args):
